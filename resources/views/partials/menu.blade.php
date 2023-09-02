d96e18bc868011edfd4bdfe3cb784222252d2706
Iman Abbasi <i.abbasi@wallex.net>
2023-09-03 02:48:35 +0330

    install jQuery

--- resources/views/partials/menu.blade.php
+++ resources/views/partials/menu.blade.php
@@ -13,35 +13,35 @@
                href="{{ $builder->getUrl($item['uri']) }}"
                class="nav-link {!! $builder->isActive($item) ? 'active' : '' !!}">
                 {!! str_repeat('&nbsp;', $depth) !!}<i class="fa fa-fw {{ $item['icon'] ?: $defaultIcon }}"></i>
-                <p>
+                <span class="ms-2">
                     {!! $builder->translate($item['title']) !!}
-                </p>
+                </span>
             </a>
         </li>
     @else
-
         <li class="{{ $horizontal ? 'dropdown' : 'has-treeview' }} {{ $depth > 0 ? 'dropdown-submenu' : '' }} nav-item {{ $builder->isActive($item) ? 'menu-open' : '' }}">
-            <a href="#"  data-id="{{ $item['id'] ?? '' }}"
+            <a href="#sidebar{{ $item['id'] ?? '' }}"  data-id="{{ $item['id'] ?? '' }}"
                class="nav-link {{ $builder->isActive($item) ? ($horizontal ? 'active' : '') : '' }}
                     {{ $horizontal ? 'dropdown-toggle' : '' }}">
                 {!! str_repeat('&nbsp;', $depth) !!}<i class="fa fa-fw {{ $item['icon'] ?: $defaultIcon }}"></i>
-                <p>
+                <span class="ms-2">
                     {!! $builder->translate($item['title']) !!}
-
                     @if(! $horizontal)
                         <i class="right fa fa-angle-left"></i>
                     @endif
-                </p>
+                </span>
             </a>
-            <ul class="nav {{ $horizontal ? 'dropdown-menu' : 'nav-treeview' }}">
-                @foreach($item['children'] as $item)
-                    @php
-                        $item['depth'] = $depth + 1;
-                    @endphp
+            <div class="collapse collapse-box" id="sidebar{{ $item['id'] ?? '' }}">
+                <ul class="nav nav-sm flex-column {{ $horizontal ? 'dropdown-menu' : 'nav-treeview' }}">
+                    @foreach($item['children'] as $item)
+                        @php
+                            $item['depth'] = $depth + 1;
+                        @endphp
 
-                    @include('admin::partials.menu', ['item' => $item])
-                @endforeach
-            </ul>
+                        @include('admin::partials.menu', ['item' => $item])
+                    @endforeach
+                </ul>
+            </div>
         </li>
     @endif
 @endif
