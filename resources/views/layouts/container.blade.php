67ea668c37ea97d5d0562c1ff25f4aafd82adc96
Iman Abbasi <i.abbasi@wallex.net>
2023-09-07 16:32:18 +0330

    Update container.blade.php

--- resources/views/layouts/container.blade.php
+++ resources/views/layouts/container.blade.php
@@ -1,12 +1,14 @@
-<body
-        class="bg-gray-100 {{ $configData['body_class']}} {{ $configData['sidebar_class'] }}
-        {{ $configData['navbar_class'] === 'fixed-top' ? 'navbar-fixed-top' : '' }} " >
+<body class="bg-gray-100 {{ $configData['body_class']}} {{ $configData['sidebar_class'] }} {{ $configData['navbar_class'] === 'fixed-top' ? 'navbar-fixed-top' : '' }} " >
+
+<script>
+    var Dcat = CreateDcat({!! Dcat\Admin\Admin::jsVariables() !!});
+</script>
 
 {!! admin_section(Dcat\Admin\Admin::SECTION['BODY_INNER_BEFORE']) !!}
 
 @include('admin::partials.sidebar')
 
-<div class="main-content">
+<div class="app-content main-content">
     @include('admin::partials.navbar')
 
     <div class="app-content" id="{{ $pjaxContainerId }}">
@@ -17,6 +19,8 @@
 {!! admin_section(Dcat\Admin\Admin::SECTION['BODY_INNER_AFTER']) !!}
 
 {!! Dcat\Admin\Admin::asset()->jsToHtml() !!}
+
+<script>Dcat.boot();</script>
 
 </body>
 
