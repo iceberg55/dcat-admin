04ab096001673fd4254fedae670ecd116109695a
Iman Abbasi <i.abbasi@wallex.net>
2023-09-09 16:20:08 +0330

    Update Grid.php

--- src/Grid.php
+++ src/Grid.php
@@ -167,7 +167,7 @@ class Grid
         'toolbar'             => true,
         'create_mode'         => self::CREATE_MODE_DEFAULT,
         'dialog_form_area'    => ['700px', '670px'],
-        'table_class'         => ['table', 'custom-data-table', 'data-table'],
+        'table_class'         => ['table', 'table-striped', 'table-hover', 'custom-data-table', 'data-table'],
         'scrollbar_x'         => false,
         'actions_class'       => null,
         'batch_actions_class' => null,
