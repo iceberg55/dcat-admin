d400370951fe2525d54bee17e5bcec90d6cebcd4
Iman Abbasi <i.abbasi@wallex.net>
2023-09-14 13:21:38 +0330

    Update sidebar-user-panel.blade.php

--- resources/views/partials/sidebar-user-panel.blade.php
+++ resources/views/partials/sidebar-user-panel.blade.php
@@ -1,10 +1,10 @@
 @if($user)
 <!-- Sidebar user panel (optional) -->
 <div class="user-panel">
-    <div class="pull-left image">
+    <div class="float-start image">
         <img style="max-height:45px" src="{{ $user->getAvatar() }}" class="img-circle">
     </div>
-    <div class="pull-left info">
+    <div class="float-start info">
         <p>{{ $user->name }}</p>
         <!-- Status -->
         <a href="#"><i class="fa fa-circle text-success"></i> {{ trans('admin.online') }}</a>
