09afe7f5dbd8ca0cffa30976fe4d94de03e7747e
Iman Abbasi <ilvlanabbasi@gmail.com>
2023-09-15 00:40:23 +0330

    Update text.blade.php

--- resources/views/filter/text.blade.php
+++ resources/views/filter/text.blade.php
@@ -6,7 +6,7 @@
         <input type="hidden" name="{{ $id }}_group" class="{{ $group_name }}-operation" value="{{ request($id.'_group', 0) }}"/>
         <ul class="dropdown-menu {{ $group_name }}">
             @foreach($group as $index => $item)
-                <li class="dropdown-item"><a  data-index="{{ $index }}"> {{ $item['label'] }} </a></li>
+                <li><a class="dropdown-item" data-index="{{ $index }}">{{ $item['label'] }}</a></li>
             @endforeach
         </ul>
     @endif
