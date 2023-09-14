e3c9eaf420260e47cf3fcb372256a17bc9556752
Iman Abbasi <i.abbasi@wallex.net>
2023-09-14 13:20:28 +0330

    Update footer.blade.php

--- resources/views/form/footer.blade.php
+++ resources/views/form/footer.blade.php
@@ -5,18 +5,18 @@
     <div class="col-md-{{$width['field']}}">
 
         @if(! empty($buttons['submit']))
-            <div class="btn-group pull-right">
+            <div class="btn-group float-end">
                 <button class="btn btn-primary submit"><i class="fas fa-save"></i>&nbsp;{{ trans('admin.submit') }}</button>
             </div>
 
             @if($checkboxes)
-                <div class="pull-right d-md-flex" style="margin:10px 15px 0 0;display: none">{!! $checkboxes !!}</div>
+                <div class="float-end d-md-flex" style="margin:10px 15px 0 0;display: none">{!! $checkboxes !!}</div>
             @endif
 
         @endif
 
         @if(! empty($buttons['reset']))
-        <div class="btn-group pull-left">
+        <div class="btn-group float-start">
             <button type="reset" class="btn btn-warning"><i class="fas fa-rotate-left"></i>&nbsp;{{ trans('admin.reset') }}</button>
         </div>
         @endif
