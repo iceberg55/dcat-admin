bc432e74c69c2709a02c962eade422350561dfb6
Iman Abbasi <ilvlanabbasi@gmail.com>
2023-09-18 03:01:40 +0330

    Update sidebar.blade.php

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -1,7 +1,11 @@
 <nav class="navbar navbar-vertical navbar-expand-lg navbar-light" id="sidebar">
     <a href="{{ admin_url('/') }}" class="navbar-brand waves-effect waves-light">
-        <img src="/storage/{!! config('admin.logo-image') !!}">
-        <img src="/storage/{!! config('admin.logo-dark-image') !!}" class="white-logo2">
+        @if( config('admin.logo-image') )
+            <img src="/storage/{!! config('admin.logo-image') !!}">
+        @endif
+        @if( config('admin.logo-dark-image') )
+            <img src="/storage/{!! config('admin.logo-dark-image') !!}" class="white-logo2">
+        @endif
     </a>
     <div class="navbar-collapse">
         <ul class="navbar-nav mb-2" id="accordionExample" data-simplebar>
