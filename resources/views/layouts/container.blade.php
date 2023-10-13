fa735f9204c0c439de0537c2a1a94257c56078af
Arvin Rahnama <arvin.rnm@gmail.com>
2023-10-14 01:05:48 +0330

    update svg pattern

--- resources/views/layouts/container.blade.php
+++ resources/views/layouts/container.blade.php
@@ -1,32 +1,19 @@
 <body class="{{ $configData['body_class']}} {{ $configData['sidebar_class'] }} {{ $configData['navbar_class'] === 'fixed-top' ? 'navbar-fixed-top' : '' }} " >
-<svg width="1760" height="850" viewBox="0 0 1565 465" fill="none" xmlns="http://www.w3.org/2000/svg"
+<svg width="1760" height="950" viewBox="0 0 1565 465" fill="none" xmlns="http://www.w3.org/2000/svg"
      class="position-absolute w-100" style="z-index: -1">
-    <path d="M990.5 463.935L990.5 413.853L1237.5 370.063V419.399L990.5 463.935Z" stroke="white" stroke-opacity="0.2"/>
-    <path d="M559 179.178L1565 0V205.19L559 387.422L559 179.178Z" fill="url(#paint0_linear_1437_18675)"/>
-    <path d="M0.500003 453.268L0.5 403.186L247.5 359.396V408.732L0.500003 453.268Z" stroke="white" stroke-opacity="0.2"/>
-    <defs>
-        <linearGradient id="paint0_linear_1437_18675" x1="1565" y1="95.8502" x2="720.927" y2="-191.482" gradientUnits="userSpaceOnUse">
-            <stop stop-color="#02a77f" stop-opacity="0.05"/>
-            <stop offset="1" stop-color="#02a77f" stop-opacity="0.15"/>
-        </linearGradient>
-    </defs>
-    <path opacity="0.2" d="M424.796 443.596L424.796 528.149L0.499817 601.602V518.301L424.796 443.596Z" stroke="#02a77f"/>
-    <path opacity="0.03" d="M660.022 561.789L147.576 651V548.838L660.022 458.106L660.022 561.789Z" fill="#02a77f"/>
-    <path opacity="0.2" d="M1619.52 424.597L1619.52 475.718L1363.52 520.405V470.045L1619.52 424.597Z" stroke="#02a77f"/>
-    <path opacity="0.2" d="M1713.52 390.597L1713.52 441.718L1457.52 486.405V436.045L1713.52 390.597Z" stroke="#02a77f"/>
-    <path opacity="0.03" d="M299 44.8613L556 -2.74093e-06V51.3741L299 97L299 44.8613Z" fill="#02a77f"/>
-    <path opacity="0.2" d="M1210.5 966.959L1210.5 898.338L1555.81 838.594V906.196L1210.5 966.959Z" stroke="#4ef09d"/>
-    <path opacity="0.05" d="M1342.73 923.622L1760 851.021V934.162L1342.73 1008L1342.73 923.622Z" fill="#4ef09d"/>
-    <path d="M203 203L1654.5 0V851.5L203 1050V203Z" fill="4ef09d"/>
-    <path d="M1454.08 103.001L1752.58 48.5604L1750.83 119.337L1452.3 174.831L1454.08 103.001Z" fill="#4ef09d"/>
-    <path opacity="0.5" d="M815.489 441.835L819.235 290.789L1451.28 175.745L1447.59 324.565L815.489 441.835Z" stroke="#4ef09d"/>
-    <path opacity="0.5" d="M104.427 901.641L427.176 850.579L423.626 926.233L100.824 978.421L104.427 901.641Z" fill="#4ef09d"/>
-    <defs>
-        <linearGradient id="paint0_linear_1428_16656" x1="203" y1="718.801" x2="1667.79" y2="510.396" gradientUnits="userSpaceOnUse">
-            <stop stop-color="#02a77f"/>
-            <stop offset="1" stop-color="#02a77f"/>
-        </linearGradient>
-    </defs>
+    <path opacity="0.2" d="M0.5 791.403L0.499997 740.282L256.5 695.595V745.955L0.5 791.403Z" stroke="#4ef09d"/>
+    <path opacity="0.03" d="M16 707.861L273 663V714.374L16 760L16 707.861Z" fill="#02a77f"/>
+    <path opacity="0.2" d="M1749.96 1113.6L1749.96 1011.31L1237.5 922.594V1023.37L1749.96 1113.6Z" stroke="#4ef09d"/>
+    <path opacity="0.2" d="M346.5 1199.4L346.5 1148.28L90.5 1103.59V1153.96L346.5 1199.4Z" stroke="#4ef09d"/>
+    <path opacity="0.03" d="M1391 44.8613L1134 -2.74093e-06V51.3741L1391 97L1391 44.8613Z" fill="#4ef09d"/>
+    <path opacity="0.2" d="M1443.23 751.404L1443.23 666.851L1867.52 593.398V676.699L1443.23 751.404Z" stroke="#02a77f"/>
+    <path opacity="0.03" d="M1208 633.211L1720.45 544V646.162L1208 736.894L1208 633.211Z" fill="#02a77f"/>
+    <path opacity="0.2" d="M235.227 207.404L235.227 122.851L659.523 49.3984V132.699L235.227 207.404Z" stroke="#02a77f"/>
+    <path opacity="0.03" d="M-6.5161e-06 89.211L512.447 -5.4506e-06V102.162L0 192.894L-6.5161e-06 89.211Z" fill="#02a77f"/>
+    <path opacity="0.2" d="M0.500003 93.3819L0.5 43.8797L206.5 0.615917V49.3807L0.500003 93.3819Z" stroke="#4ef09d"/>
+    <path opacity="0.2" d="M1164.5 129.139L1164.5 60.4214L1510.09 0.593993V68.2915L1164.5 129.139Z" stroke="#02a77f"/>
+    <path opacity="0.05" d="M1296.83 85.7403L1714.44 13.0389V96.2948L1296.83 170.235L1296.83 85.7403Z" fill="#02a77f"/>
+    <path opacity="0.2" d="M0.5 559.6L0.499994 457.309L512.957 368.594V469.373L0.5 559.6Z" stroke="#02a77f"/>
 </svg>
 <script>
     var Dcat = CreateDcat({!! Dcat\Admin\Admin::jsVariables() !!});
