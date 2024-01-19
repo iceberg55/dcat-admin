220e6f85cb5c0fcb0094c093a8d274303816a869
Iman Abbasi <ilvlanabbasi@gmail.com>
2024-01-19 14:23:45 +0330

    Create CreditService.php

---
+++ src/Services/CreditService.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace Dcat\Admin\Services;
+
+use Dcat\Admin\Exception\CreditException;
+use Dcat\Admin\Models\Credit;
+use Dcat\Admin\Models\Transaction;
+use Dcat\Admin\Admin;
+use Exception;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Database\Eloquent\Model;
+
+class CreditService
+{
+    /**
+     * @param Model $owner
+     * @return int|float
+     */
+    public function getUserCredit(Model $owner): int|float
+    {
+        try {
+            $credit = Credit::where([
+                ['owner_type', $owner::class],
+                ['owner_id', $owner->getKey()],
+            ])->firstOrFail();
+
+            return $credit->balance;
+        } catch (Exception $exception) {
+            return 0;
+        }
+    }
+
+    /**
+     * @return float|int
+     * @throws AuthenticationException
+     */
+    public function getCurrentUserCredit(): float|int
+    {
+        if (! Admin::user()) {
+            throw new AuthenticationException();
+        }
+
+        return $this->getUserCredit(Admin::user());
+    }
+
+    /**
+     * @param Model $owner
+     * @return void
+     * @throws CreditException
+     */
+    public function updateUserCredit(Model $owner): void
+    {
+        $balance = 0;
+
+        foreach (Transaction::POSITIVE_TYPES as $type) {
+            $balance += Transaction::where([
+                ['owner_type', $owner::class],
+                ['owner_id', $owner->getKey()],
+                ['type', $type],
+                ['status', Transaction::STATUS_SUCCESS],
+            ])->get()->sum('amount');
+        }
+        foreach (Transaction::NEGATIVE_TYPES as $type) {
+            $balance -= Transaction::where([
+                ['owner_type', $owner::class],
+                ['owner_id', $owner->getKey()],
+                ['type', $type],
+                ['status', Transaction::STATUS_SUCCESS],
+            ])->get()->sum('amount');
+        }
+
+        $credit = Credit::firstOrCreate([
+            'owner_type' => $owner::class,
+            'owner_id' => $owner->getKey(),
+        ]);
+        $credit->balance = $balance;
+        $credit->save();
+
+        if( $credit->balance < 0 ){
+            throw new CreditException('Credit must be greater or equal zero !');
+        }
+    }
+}
